bbf38b4f89bedf71b276c7281c52b52fe0e13949
lilinhai <lilinhai548@163.com>
2018-02-23 22:33:35 +0800

    g

--- nature-j2ee/nature-j2ee-generator/src/main/java/pers/linhai/nature/j2ee/generator/core/internal/types/JavaTypeResolverDefaultImpl.java
+++ nature-j2ee/nature-j2ee-generator/src/main/java/pers/linhai/nature/j2ee/generator/core/internal/types/JavaTypeResolverDefaultImpl.java
@@ -115,7 +115,7 @@ public class JavaTypeResolverDefaultImpl implements JavaTypeResolver {
         typeMap.put(Types.TIMESTAMP, new JdbcTypeInformation("TIMESTAMP", //$NON-NLS-1$
                 new FullyQualifiedJavaType(Date.class.getName())));
         typeMap.put(Types.TINYINT, new JdbcTypeInformation("TINYINT", //$NON-NLS-1$
-                new FullyQualifiedJavaType(Byte.class.getName())));
+                new FullyQualifiedJavaType(Integer.class.getName())));
         typeMap.put(Types.VARBINARY, new JdbcTypeInformation("VARBINARY", //$NON-NLS-1$
                 new FullyQualifiedJavaType("byte[]"))); //$NON-NLS-1$
         typeMap.put(Types.VARCHAR, new JdbcTypeInformation("VARCHAR", //$NON-NLS-1$
