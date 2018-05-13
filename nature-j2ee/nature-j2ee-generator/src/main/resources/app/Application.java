08eea33370b675a4f38fd69aa4978c384cac6f10
lilinhai <lilinhai548@163.com>
2018-05-13 10:38:28 +0800

    21

--- nature-j2ee/nature-j2ee-generator/src/main/resources/app/Application.java
+++ nature-j2ee/nature-j2ee-generator/src/main/resources/app/Application.java
@@ -7,7 +7,6 @@ import pers.linhai.nature.j2ee.core.BaseApplication;
 /**
  * Spring boot应用启动类
  */
-@SpringBootApplication
 public class Application extends BaseApplication
 {
     public static void main( String[] args )
