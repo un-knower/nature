5cf7da11d5f84d7f8387ef97a0c444026c31fa36
lilinhai <lilinhai548@163.com>
2018-05-14 18:13:28 +0800

    21

--- nature-j2ee/nature-j2ee-core/src/main/java/pers/linhai/nature/j2ee/core/common/ExcelParser.java
+++ nature-j2ee/nature-j2ee-core/src/main/java/pers/linhai/nature/j2ee/core/common/ExcelParser.java
@@ -17,6 +17,8 @@ import java.util.List;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * <p>Description    : <pre>TODO(这里用一句话描述这个类的作用)</pre></p>
@@ -27,13 +29,49 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ExcelParser
 {
     
+    private static final List<Constructor< ? extends Workbook>> WORK_BOOK_CONSTRUCTOR_LIST = new ArrayList<Constructor< ? extends Workbook>>(2);
+    static
+    {
+        try
+        {
+            WORK_BOOK_CONSTRUCTOR_LIST.add(XSSFWorkbook.class.getConstructor(InputStream.class));
+            WORK_BOOK_CONSTRUCTOR_LIST.add(HSSFWorkbook.class.getConstructor(InputStream.class));
+        }
+        catch (Throwable e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * 日志记录器
+     */
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    
+    private Workbook workbook;
+    
+    public ExcelParser(InputStream source)
+    {
+        for (Constructor< ? extends Workbook> constructor : WORK_BOOK_CONSTRUCTOR_LIST)
+        {
+            try
+            {
+                workbook = constructor.newInstance(new FileInputStream("C:\\Users\\lilinhai\\Desktop\\test\\test.xls"));
+                break;
+            }
+            catch (Throwable e)
+            {
+                logger.error("ExcelParser(InputStream source) occor an error.", e);
+            }
+        }
+    }
+    
+    
+    
     public static void main(String[] args) throws Exception
     {
-        List<Constructor< ? extends Workbook>> workbookConstructorList = new ArrayList<Constructor< ? extends Workbook>>(2);
-        workbookConstructorList.add(XSSFWorkbook.class.getConstructor(InputStream.class));
-        workbookConstructorList.add(HSSFWorkbook.class.getConstructor(InputStream.class));
         Workbook hssWB = null;
-        for (Constructor< ? extends Workbook> constructor : workbookConstructorList)
+        for (Constructor<? extends Workbook> constructor : WORK_BOOK_CONSTRUCTOR_LIST)
         {
             try
             {
@@ -46,6 +84,14 @@ public class ExcelParser
             }
         }
         hssWB.close();
+       
+        
+        
+        
+        
+        
+        
+        
         
     }
 }
