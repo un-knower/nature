ec26969f2383c7a46b75cb7bd26aeeca009926b3
lilinhai <lilinhai548@163.com>
2018-06-11 15:30:17 +0800

    432

--- nature-j2ee/nature-j2ee-generator/src/main/resources/app/script/application-start.bat
+++ nature-j2ee/nature-j2ee-generator/src/main/resources/app/script/application-start.bat
@@ -1,26 +1,36 @@
 @echo off&color 0E&Title ${artifactId}-application-console
-if exist "%JAVA_HOME%" (
-    echo Detection of a running Java environment 
-	set PATH=%JAVA_HOME%\bin;%PATH%
-	goto findJdk
-) else (
-    set curdir=%~dp0
-    cd ../
-    echo The operating system does not install the Java running environment, and begin to find java-home from installation package
-    goto getJdkHome
-)
 
-:getJdkHome
-for /f "delims=" %%i in ('dir /ad /b "%cd%" ^| findstr "jdk.*$"') do (
-	set JDK_HOME=%%~fi
-	if exist "%JDK_HOME%" (
-	    goto findJdk
+rem Detects whether the java_home environment variable is defined 
+if defined JAVA_HOME (
+	if exist "%JAVA_HOME%" (
+		echo Detection of a running Java environment 
+		set PATH=%JAVA_HOME%\bin;%PATH%
+		goto findJavaHome
 	)
 )
+set curdir=%~dp0
+cd ../
+echo The operating system does not install the Java running environment, and begin to find java-home from installation package
+goto getJdkHome
 
-:findJdk
-echo Find JDK in the installation package directory: %JDK_HOME%
-set PATH=%JDK_HOME%\bin;%PATH%
+:getJdkHome
+for /f "delims=" %%i in ('dir /ad /b "%cd%" ^| findstr "^jdk.*$"') do (
+	set JAVA_HOME=%%~fi
+	goto checkIsFindJavaHome
+)
+
+:checkIsFindJavaHome
+if defined JAVA_HOME (
+	if exist %JAVA_HOME% (
+		echo Find JDK in the installation package directory: %JAVA_HOME%
+		set PATH=%JAVA_HOME%\bin;%PATH%
+		goto findJavaHome
+	)
+)
+echo Can't find the JAVA_HOME in the installation package directory.
+pause
+
+:findJavaHome
 echo %PATH%
 java -version
 for /f "delims=" %%i in ('dir /b /s "%curdir%../lib/${artifactId}*application*.jar" ^| findstr "${artifactId}\-application\-.*.jar$"') do (
